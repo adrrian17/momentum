33c5c0e026a5b67c15ca3d452b0bd3460c466bae
Adrian Ayala <adrian.ayala17@gmail.com>
2022-02-22 14:01:33 -0600

    Add a custom navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 module.exports = {
-  content: [
-    './*.hbs',
-    './**/*.hbs',
-    './assets/css/*.css',
-    './assets/css/**/*.css',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./*.hbs',
+		'./**/*.hbs',
+		'./assets/css/*.css',
+		'./assets/css/**/*.css',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				'work-sans': ['"Work Sans"'],
+			},
+			colors: {
+				brand: {
+					50: '#e2eeff',
+					100: '#b3ccff',
+					200: '#83aafd',
+					300: '#5288f9',
+					400: '#2366f6',
+					500: '#094cdc',
+					600: '#023bac',
+					700: '#002a7c',
+					800: '#00194d',
+					900: '#00081f',
+				},
+			},
+		},
+	},
+	plugins: [],
 };
